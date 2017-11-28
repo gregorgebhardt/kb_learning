b1703c187293fc8b03d6b35cdf7db2822e8a7bf4
Gregor Gebhardt <gebhardt@ias.tu-darmstadt.de>
2017-11-28 14:25:35 +0100

    initialization of light and Kilobots is now checked against the bounds of the table and Kilobots are only spawned in the range of the circular light.

--- kb_learning/envs/single_quad.py
+++ kb_learning/envs/single_quad.py
@@ -32,10 +32,12 @@ class QuadPushingEnv(KilobotsEnv):
     _object_init = np.array([.0, .0, .0])
     _object_size = .2
 
+    _light_radius = .2
+
     _spawn_type_ratio = -.95
     # _sampling_max_radius = .5
-    _spawn_radius_variance = 1.
-    _light_max_dist = .75
+    _spawn_radius_variance = .4 * _light_radius
+    _light_max_dist = .75 * min(world_size)
     _spawn_angle_mean = np.pi
     _spawn_angle_variance = np.pi / 4
 
@@ -66,48 +68,49 @@ class QuadPushingEnv(KilobotsEnv):
     def _configure_environment(self):
         # initialize object always at (0, 0) with 0 orientation (we do not need to vary the object position and
         # orientation since we will later adapt the state based on the object pose.)
-        pushing_object = CornerQuad(width=self._object_size, height=self._object_size,
-                                    position=self._object_init[:2], orientation=self._object_init[2],
-                                    world=self.world)
-        self._objects.append(pushing_object)
+        self._objects.append(CornerQuad(width=self._object_size, height=self._object_size,
+                                        position=self._object_init[:2], orientation=self._object_init[2],
+                                        world=self.world))
 
         # determine the sampling mode for this episode
-        spawn_randomly = np.random.rand() < np.abs(self._spawn_type_ratio)
+        _spawn_randomly = np.random.rand() < np.abs(self._spawn_type_ratio)
 
-        if spawn_randomly:
+        if _spawn_randomly:
             # if we spawn the light randomly, we select random polar coordinates for the light
             # the radius is half the object width plus a uniform sample from the range [0, light_max_dist)
-            radius = (0.5 * self._object_size + self._light_max_dist * np.random.rand())
+            _kb_radius = (0.5 * self._object_size + self._light_max_dist * np.random.rand())
             # the angle is sampled normally with spawn_angle_mean and spawn_angle_variance
             angle = np.random.normal(self._spawn_angle_mean, self._spawn_angle_variance)
 
-            _light_init = self._object_init[:2] + radius * np.array([np.cos(angle), np.sin(angle)])
+            _light_init = self._object_init[:2] + _kb_radius * np.array([np.cos(angle), np.sin(angle)])
+            _light_init = np.minimum(_light_init, self.world_bounds[0] + self._light_radius)
+            _light_init = np.maximum(_light_init, self.world_bounds[1] - self._light_radius)
         else:
             # otherwise, the light starts above the object
             _light_init = self._object_init[:2]
 
         # initialize light position
-        self._light = CircularGradientLight(position=_light_init, radius=.4, bounds=self.world_bounds)
+        self._light = CircularGradientLight(position=_light_init, radius=self._light_radius, bounds=self.world_bounds)
 
         # kilobots start at the light position in a slightly random formation
-        radius = PhototaxisKilobot.get_radius()
-        kilobot_offsets = np.array([[-radius, 0], [radius, 0], [0, radius], [0, -radius]])
-        start_angles = 2 * np.pi * np.random.randn(self._num_kilobots)
+        _kb_radius = PhototaxisKilobot.get_radius()
+        _kilobot_offsets = np.array([[-_kb_radius, 0], [_kb_radius, 0], [0, _kb_radius], [0, -_kb_radius]])
+        _start_angles = 2 * np.pi * np.random.randn(self._num_kilobots)
 
         for i in range(self._num_kilobots):
-            if spawn_randomly:
-                position = _light_init + (10 + i / 4) * kilobot_offsets[i % 4, :] + .25 * (np.random.rand(2) - .5)
+            if _spawn_randomly:
+                _position = _light_init + (1 + i / 4) * _kilobot_offsets[i % 4, :] + .25 * (np.random.rand(2) - .5)
                 # x = (start[0] + (10 + i / 4) * kilobot_offsets[i % 4, 0] + 0.25 * (np.random.rand() - 0.5))
                 # y = (start[1] + (10 + i / 4) * kilobot_offsets[i % 4, 1] + 0.25 * (np.random.rand() - 0.5))
             else:
-                sampled_radius = np.maximum(np.random.normal(0, self._spawn_radius_variance, 1), 1.1 * radius)
+                _sampled_radius = np.maximum(np.random.normal(0, self._spawn_radius_variance), 1.1 * _kb_radius)
+                _sampled_radius = np.minimum(_sampled_radius, self._light_radius)
                 # x = float(start[0] + 1.1 * sampled_radius * np.cos(start_angles[i]))
                 # y = float(start[1] + 1.1 * sampled_radius * np.sin(start_angles[i]))
-                position = _light_init + 1.1 * sampled_radius * np.array([np.cos(start_angles[i]),
-                                                                          np.sin(start_angles[i])])
+                _position = _light_init + _sampled_radius * np.r_[np.cos(_start_angles[i]), np.sin(_start_angles[i])]
 
             # the orientation of the kilobots is chosen randomly
-            orientation = 2 * np.pi * np.random.rand()
+            _orientation = 2 * np.pi * np.random.rand()
 
-            self._add_kilobot(PhototaxisKilobot(self.world, position=position, orientation=orientation,
+            self._add_kilobot(PhototaxisKilobot(self.world, position=_position, orientation=_orientation,
                                                 light=self._light))
