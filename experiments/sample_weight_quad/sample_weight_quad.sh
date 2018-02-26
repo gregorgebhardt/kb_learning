8e3b7fc582a13453bcc94db697d996621ac2e88b
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-02-26 23:48:25 +0100

    prepared experiments for cluster

--- experiments/sample_weight_quad/sample_weight_quad.sh
+++ experiments/sample_weight_quad/sample_weight_quad.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH -A project00664 # 672
 #SBATCH -J sample_weight_quad
-#SBATCH -D /home/yy05vipo/ftp/kb_learning/experiments
+#SBATCH -D /home/yy05vipo/git/kb_learning/experiments
 #SBATCH --mail-type=END
 # Please use the complete path details :
-#SBATCH -e /home/yy05vipo/ftp/kb_learning/experiments/sample_weight_quad/l_%j.stderr
-#SBATCH -o /home/yy05vipo/ftp/kb_learning/experiments/sample_weight_quad/l_%j.stdout
+#SBATCH -e /home/yy05vipo/git/kb_learning/experiments/sample_weight_quad/l_%j.stderr
+#SBATCH -o /home/yy05vipo/git/kb_learning/experiments/sample_weight_quad/l_%j.stdout
 #
 #SBATCH -n 15               # Number of tasks
 #SBATCH -c 8                # Number of cores per task
 #SBATCH --mem-per-cpu=500   # Main memory in MByte per MPI task
-#SBATCH -t 8:00:00          # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
+#SBATCH -t 6:00:00          # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
 ### SBATCH --hint=multithread
 
 # -------------------------------
@@ -20,7 +20,7 @@
 #module load gcc/4.9.4 openmpi/gcc/2.1.2 python/3.6.2 intel/2018u1 boost/1.61
 
 source /home/yy05vipo/.virtenvs/gym/bin/activate
-cd /home/yy05vipo/ftp/kb_learning/experiments
+cd /home/yy05vipo/git/kb_learning/experiments
 
 srun hostname > $SLURM_JOB_ID.hostfile
 hostfileconv $SLURM_JOB_ID.hostfile -1
