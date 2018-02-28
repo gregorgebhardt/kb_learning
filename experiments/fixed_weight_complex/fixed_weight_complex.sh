de42f3a23049d2729097c883f1095b92895c7283
Gregor Gebhardt <gebhardt@ias.tu-darmstadt.de>
2018-02-28 17:52:20 +0100

    changed no of tasks and time in fixed_weights_complex.

--- experiments/fixed_weight_complex/fixed_weight_complex.sh
+++ experiments/fixed_weight_complex/fixed_weight_complex.sh
@@ -7,10 +7,10 @@
 #SBATCH -e /home/yy05vipo/git/kb_learning/experiments/fixed_weight_complex/l_%j.stderr
 #SBATCH -o /home/yy05vipo/git/kb_learning/experiments/fixed_weight_complex/l_%j.stdout
 #
-#SBATCH -n 30               # Number of tasks
+#SBATCH -n 45               # Number of tasks
 #SBATCH -c 8                # Number of cores per task
 #SBATCH --mem-per-cpu=500   # Main memory in MByte per MPI task
-#SBATCH -t 12:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
+#SBATCH -t 4:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
 ### SBATCH --hint=multithread
 
 # -------------------------------
