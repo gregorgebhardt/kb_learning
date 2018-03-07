e2babc64d8694ed79a9b98f489f7653e33ccf71e
Gregor Gebhardt <gebhardt@ias.tu-darmstadt.de>
2018-03-07 20:55:08 +0100

    added gradient_light experiment and removed unused classes.

---
+++ experiments/gradient_light_complex/gradient_light_complex.py
@@ -0,0 +1,4 @@
+from kb_learning.learner import GradientLightComplexObjectACRepsLearner
+
+if __name__ == '__main__':
+    GradientLightComplexObjectACRepsLearner.run()
---
+++ experiments/gradient_light_complex/gradient_light_complex.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A project00672 # 672, 664
+#SBATCH -J gradient_light_complex
+#SBATCH -D /home/yy05vipo/git/kb_learning/experiments
+#SBATCH --mail-type=END
+# Please use the complete path details :
+#SBATCH -e /home/yy05vipo/git/kb_learning/experiments/gradient_light_complex/l_%j.stderr
+#SBATCH -o /home/yy05vipo/git/kb_learning/experiments/gradient_light_complex/l_%j.stdout
+#
+#SBATCH -n 45               # Number of tasks
+#SBATCH -c 8                # Number of cores per task
+#SBATCH --mem-per-cpu=500   # Main memory in MByte per MPI task
+#SBATCH -t 2:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
+#SBATCH --hint=multithread
+
+# -------------------------------
+# Afterwards you write your own commands, e.g.
+
+#module purge
+#module load gcc/4.9.4 openmpi/gcc/2.1.2 python/3.6.2 intel/2018u1 boost/1.61
+
+source /home/yy05vipo/.virtenvs/gym/bin/activate
+cd /home/yy05vipo/git/kb_learning/experiments
+
+srun hostname > $SLURM_JOB_ID.hostfile
+hostfileconv $SLURM_JOB_ID.hostfile -1
+job_stream --hostfile $SLURM_JOB_ID.hostfile.converted -- python gradient_light_complex/gradient_light_complex.py -c gradient_light_complex/gradient_light_complex.yml --log_level INFO
