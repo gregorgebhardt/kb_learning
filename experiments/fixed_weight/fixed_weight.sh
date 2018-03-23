4cb6e448870fe5c5d13e309427c86982fb9db3c1
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-03-23 01:43:28 +0100

    fixed sbatch script for fixed_weight

--- experiments/fixed_weight/fixed_weight.sh
+++ experiments/fixed_weight/fixed_weight.sh
@@ -4,13 +4,13 @@
 #SBATCH -D /home/yy05vipo/git/kb_learning/experiments
 #SBATCH --mail-type=END
 # Please use the complete path details :
-#SBATCH -e /home/yy05vipo/git/kb_learning/experiments/fixed_weight_complex/l_%j.stderr
-#SBATCH -o /home/yy05vipo/git/kb_learning/experiments/fixed_weight_complex/l_%j.stdout
+#SBATCH -e /home/yy05vipo/git/kb_learning/experiments/fixed_weight/l_%j.stderr
+#SBATCH -o /home/yy05vipo/git/kb_learning/experiments/fixed_weight/l_%j.stdout
 #
 #SBATCH -n 27               # Number of tasks
 #SBATCH -c 8                # Number of cores per task
 #SBATCH --mem-per-cpu=1000   # Main memory in MByte per MPI task
-#SBATCH -t 3:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
+#SBATCH -t 6:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
 #SBATCH -C avx2            # requires new nodes
 ### SBATCH --hint=multithread
 
@@ -25,4 +25,8 @@ cd /home/yy05vipo/git/kb_learning/experiments
 
 srun hostname > $SLURM_JOB_ID.hostfile
 hostfileconv $SLURM_JOB_ID.hostfile -1
-job_stream --hostfile $SLURM_JOB_ID.hostfile.converted -- python fixed_weight_complex/fixed_weight_complex.py -c fixed_weight_complex/fixed_weight_complex.yml --log_level INFO -e bandwidth_eval -o
+
+job_stream --hostfile $SLURM_JOB_ID.hostfile.converted -- python fixed_weight/fixed_weight.py -c fixed_weight/fixed_weight.yml --log_level INFO -e bandwidth_eval -o
+
+rm $SLURM_JOB_ID.hostfile
+rm $SLURM_JOB_ID.hostfile.converted
