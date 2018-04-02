fixed bugs in kernel gradients, added bandwidth estimator for kilobots, adaptions to sampler for GPy implementation

kb_learning/ac_reps/gpy_spwgp.py
@@ -3,6 +3,7 @@ import GPy
 
 from paramz.transformations import Logexp
 from GPy.core.parameterization import Param
+from kb_learning.kernel import KilobotEnvKernel
 
 
 class SemiHeteroscedasticGaussian(GPy.likelihoods.Gaussian):
@@ -18,31 +19,99 @@ class SemiHeteroscedasticGaussian(GPy.likelihoods.Gaussian):
         self.het_variance = Param('het_variance', het_variance, Logexp())
         self.link_parameter(self.het_variance)
         self.het_variance.fix()
+        self.variance.fix()
 
     def gaussian_variance(self, Y_metadata=None):
         return self.het_variance[Y_metadata['output_index'].flatten()] * self.variance
 
 
 class SparseWeightedGPyWrapper:
-    def __init__(self, X, Y, Z, weights, kernel, noise_var=1., mean_function=None):
-        semi_het_lik = SemiHeteroscedasticGaussian(variance=noise_var, het_variance=1 / weights)
-        if mean_function:
-            Y = Y - mean_function(X)
+    def __init__(self, kernel, noise_var=1., mean_function=None, output_bounds=None):
+        self.kernel = kernel
+        # self.kernel.variance.fix()
+        self.noise_var = noise_var
 
-        self._gp = GPy.core.SparseGP(X=X, Y=Y, Z=Z, kernel=kernel, likelihood=semi_het_lik,
-                                     Y_metadata=dict(output_index=np.arange(100)))
+        self._gp = None
         self._mean_function = mean_function
 
-        self.output_bounds = None
-        self.output_bounds_enforce = False
+        self.output_bounds = output_bounds
 
-    def get_mean(self, X):
-        return self._gp.predict(Xnew=X)[0]
-
-    def get_mean_sigma(self, X):
-        return self._gp.predict(Xnew=X)
-
-    def sample(self, X, num_samples_per_X=1):
+    def train(self, inputs, outputs, weights, sparse_inputs, optimize=True):
         if self._mean_function:
-            return self._mean_function(X) + self._gp.posterior_samples(X, size=1)
-        return self._gp.posterior_samples(X, size=num_samples_per_X)
+            outputs = outputs - self._mean_function(inputs)
+
+        if self._gp is None:
+            semi_het_lik = SemiHeteroscedasticGaussian(variance=self.noise_var, het_variance=weights.max() / weights)
+            self._gp = GPy.core.SparseGP(X=inputs, Y=outputs, Z=sparse_inputs, kernel=self.kernel,
+                                         likelihood=semi_het_lik,
+                                         Y_metadata=dict(output_index=np.arange(inputs.shape[0])))
+            self._gp.inducing_inputs.fix()
+
+        else:
+            # self._gp.set_updates(False)
+            self._gp.set_XY(inputs, outputs)
+            self._gp.set_Z(sparse_inputs, False)
+            self._gp.inducing_inputs.fix()
+            self._gp.likelihood.het_variance = weights.max() / weights
+            # self._gp.set_updates()
+            self._gp.update_model(True)
+
+        if optimize:
+            self._gp.optimize('bfgs', messages=True)
+        print(self._gp)
+
+    def to_dict(self):
+        input_dict = dict()
+        input_dict['kernel'] = self.kernel.to_dict()
+        input_dict['noise_var'] = self.noise_var
+        input_dict['mean_function'] = self._mean_function
+        input_dict['output_bounds'] = self.output_bounds
+
+        if self._gp:
+            input_dict['gp'] = dict()
+            input_dict['gp']['inputs'] = self._gp.X.values
+            input_dict['gp']['outputs'] = self._gp.Y.values
+            input_dict['gp']['sparse_inputs'] = self._gp.Z.values
+            input_dict['gp']['weights'] = 1 / self._gp.likelihood.het_variance.values
+
+        return input_dict
+
+    @staticmethod
+    def from_dict(input_dict):
+        gp_dict = input_dict.pop('gp', None)
+        kernel_dict = input_dict.pop('kernel')
+        input_dict['kernel'] = KilobotEnvKernel.from_dict(kernel_dict)
+
+        spwgp = SparseWeightedGPyWrapper(**input_dict)
+        if gp_dict:
+            spwgp.train(**gp_dict, optimize=False)
+
+        return spwgp
+
+    def get_mean(self, inputs):
+        return self._gp.predict(Xnew=inputs)[0]
+
+    def get_mean_sigma(self, inputs):
+        return self._gp.predict(Xnew=inputs)
+
+    def sample(self, inputs):
+        if self._gp is None:
+            variance = self.noise_var + self.kernel.variance
+            # TODO remove magic number
+            samples = np.random.normal(scale=np.sqrt(variance), size=(inputs.shape[0], 2))
+        else:
+            mean, variance = self._gp.predict(Xnew=inputs)
+            samples = mean + np.random.normal(scale=np.sqrt(variance), size=(inputs.shape[0], 2))
+
+        if self._mean_function:
+            samples += self._mean_function(inputs)
+
+        if self.output_bounds is not None:
+                # check samples against bounds from action space
+            samples = np.minimum(samples, self.output_bounds[1])
+            samples = np.maximum(samples, self.output_bounds[0])
+
+        return samples
+
+    def __call__(self, inputs):
+        return self.sample(inputs)

kb_learning/ac_reps/spwgp.py
@@ -123,9 +123,9 @@ class SparseWeightedGP:
         gp_mean, gp_sigma = self.get_mean_sigma(inputs)
 
         if gp_mean.ndim > 1:
-            samples = np.random.normal(gp_mean, gp_sigma[:, None])
+            samples = np.random.normal(gp_mean, np.sqrt(gp_sigma[:, None]))
         else:
-            samples = np.random.normal(gp_mean, gp_sigma)
+            samples = np.random.normal(gp_mean, np.sqrt(gp_sigma))
 
         if self.output_bounds_enforce:
             # check samples against bounds from action space

kb_learning/envs/sampler.py
@@ -8,6 +8,8 @@ import pandas as pd
 import gym
 import logging
 
+from kb_learning.ac_reps.gpy_spwgp import SparseWeightedGPyWrapper
+
 logger = logging.getLogger('kb_learning.sampler')
 
 
@@ -92,7 +94,7 @@ class SARSSampler(ObjectEnvSampler):
 
         observation_dims = states.shape[1]
         action_dims = sum(self.env.action_space.shape)
-        state_dims = sum(sum(s.shape) for s in envs[0].state_space.spaces)
+        state_dims = sum(sum(s.shape) for s in self.envs[0].state_space.spaces)
 
         it_sars_data = np.empty((num_episodes * num_steps_per_episode, 2 * observation_dims + action_dims + 1))
         it_info_data = np.empty((num_episodes * num_steps_per_episode, state_dims))
@@ -135,7 +137,7 @@ def _set_worker_seed(seed, work_seed):
     np.random.seed(work_seed * 10000 + seed * 400 + 12346)
 
 
-def _do_work(policy, num_episodes, num_steps, seed, work_seed):
+def _do_work(policy_dict, num_episodes, num_steps, seed, work_seed):
     global envs
     _set_worker_seed(seed, work_seed)
     # reset environments and obtain initial states
@@ -150,6 +152,7 @@ def _do_work(policy, num_episodes, num_steps, seed, work_seed):
     it_sars_data = np.empty((num_episodes * num_steps, 2 * observation_dims + action_dims + 1))
     it_info_data = np.empty((num_episodes * num_steps, state_dims))
 
+    policy = SparseWeightedGPyWrapper.from_dict(policy_dict)
     # do one additional step before
     actions = policy(states)
     srdi = [e.step(a) for e, a in zip(envs[:num_episodes], actions)]
@@ -221,7 +224,7 @@ class ParallelSARSSampler(SARSSampler):
 
             # construct work packages with policy, number of episodes, number of steps, seed and work-seed,
             # where work-seed is is taken from the range of work packages
-            work = [(policy, episodes, num_steps_per_episode, self._seed, work_seed) for episodes, work_seed
+            work = [(policy.to_dict(), episodes, num_steps_per_episode, self._seed, work_seed) for episodes, work_seed
                     in zip(episodes_per_work, range(len(episodes_per_work)))]
 
             for i in range(self._num_restarts):

kb_learning/kernel/_kernel.py
@@ -7,42 +7,62 @@ from . import EmbeddedSwarmDistance, MahaDist
 
 
 class KilobotEnvKernel(Kern):
-    def __init__(self, kilobots_dim, light_dim=0, weight_dim=0, action_dim=0,
-                 rho=.5, active_dims=None):
+    _name = 'kilobot_env'
+
+    def __init__(self, kilobots_dim, light_dim=0, weight_dim=0, action_dim=0, rho=.5, variance=1.,
+                 kilobots_bandwidth=None, light_bandwidth=None, weight_bandwidth=None,
+                 action_bandwidth=None, light_dist_class=None, weight_dist_class=None, action_dist_class=None,
+                 active_dims=None):
         super(KilobotEnvKernel, self).__init__(input_dim=kilobots_dim+light_dim+weight_dim+action_dim,
-                                               active_dims=active_dims, name='kilobot_env')
+                                               active_dims=active_dims, name=self._name)
         self.kilobots_dim = kilobots_dim
         self.light_dim = light_dim
         self.weight_dim = weight_dim
         self.action_dim = action_dim
 
         self.kilobots_dist = EmbeddedSwarmDistance()
-        self.light_dist = MahaDist()
-        self.weight_dist = MahaDist()
-        self.action_dist = MahaDist()
+        self.light_dist = light_dist_class() if light_dist_class else MahaDist()
+        self.weight_dist = weight_dist_class() if weight_dist_class else MahaDist()
+        self.action_dist = action_dist_class() if action_dist_class else MahaDist()
 
-        self.kilobots_bandwidth = Param('kilobots_bandwidth', np.array([1.] * 2), Logexp())
+        if kilobots_bandwidth is None:
+            self.kilobots_bandwidth = Param('kilobots_bandwidth', np.array([1.] * 2), Logexp())
+        else:
+            self.kilobots_bandwidth = Param('kilobots_bandwidth', kilobots_bandwidth, Logexp())
+        self.kilobots_dist.bandwidth = self.kilobots_bandwidth
         self.kilobots_bandwidth.add_observer(self, self.__kilobots_bandwidth_observer)
         self.link_parameter(self.kilobots_bandwidth)
 
         if light_dim:
-            self.light_bandwidth = Param('light_bandwidth', np.array([1.] * light_dim), Logexp())
+            if light_bandwidth is None:
+                self.light_bandwidth = Param('light_bandwidth', np.array([1.] * light_dim), Logexp())
+            else:
+                self.light_bandwidth = Param('light_bandwidth', light_bandwidth, Logexp())
+            self.light_dist.bandwidth = self.light_bandwidth
             self.light_bandwidth.add_observer(self, self.__light_bandwidth_observer)
             self.link_parameter(self.light_bandwidth)
 
         if weight_dim:
-            self.weight_bandwidth = Param('weight_bandwidth', np.array([1.] * weight_dim), Logexp())
+            if weight_bandwidth is None:
+                self.weight_bandwidth = Param('weight_bandwidth', np.array([1.] * weight_dim), Logexp())
+            else:
+                self.weight_bandwidth = Param('weight_bandwidth', weight_bandwidth, Logexp())
+            self.weight_dist.bandwidth = self.weight_bandwidth
             self.weight_bandwidth.add_observer(self, self.__weight_bandwidth_observer)
             self.link_parameter(self.weight_bandwidth)
 
         if action_dim:
-            self.action_bandwidth = Param('action_bandwidth', np.array([1.] * action_dim), Logexp())
+            if action_bandwidth is None:
+                self.action_bandwidth = Param('action_bandwidth', np.array([1.] * action_dim), Logexp())
+            else:
+                self.action_bandwidth = Param('action_bandwidth', action_bandwidth, Logexp())
+            self.action_dist.bandwidth = self.action_bandwidth
             self.action_bandwidth.add_observer(self, self.__action_bandwidth_observer)
             self.link_parameter(self.action_bandwidth)
 
         self.rho = Param('rho', np.array([rho]))
         self.rho.constrain_bounded(.0, 1.)
-        self.variance = Param('variance', np.array([1.]), Logexp())
+        self.variance = Param('variance', np.array([variance]), Logexp())
 
         self.link_parameters(self.rho, self.variance)
 
@@ -57,6 +77,31 @@ class KilobotEnvKernel(Kern):
 
     def __action_bandwidth_observer(self, param, which):
         self.action_dist.bandwidth = self.action_bandwidth.values
+
+    def to_dict(self):
+        input_dict = dict()
+        input_dict['kilobots_dim'] = self.kilobots_dim
+        input_dict['light_dim'] = self.light_dim
+        input_dict['weight_dim'] = self.weight_dim
+        input_dict['action_dim'] = self.action_dim
+        input_dict['kilobots_bandwidth'] = self.kilobots_bandwidth.values
+        if self.light_dim:
+            input_dict['light_bandwidth'] = self.light_bandwidth.values
+        if self.weight_dim:
+            input_dict['weight_bandwidth'] = self.weight_bandwidth.values
+        if self.action_dim:
+            input_dict['action_bandwidth'] = self.action_bandwidth.values
+        # TODO add distance classes
+        input_dict['rho'] = self.rho[0]
+        input_dict['variance'] = self.variance[0]
+
+        return input_dict
+
+    @staticmethod
+    def from_dict(input_dict):
+        import copy
+        input_dict = copy.deepcopy(input_dict)
+        return KilobotEnvKernel(**input_dict)
 
     def K(self, X, X2=None, return_components=False):
         X_splits = np.split(X, np.cumsum([self.kilobots_dim, self.light_dim, self.weight_dim]), axis=1)
@@ -77,24 +122,24 @@ class KilobotEnvKernel(Kern):
             Y_weight = None
             Y_action = None
 
-        k_kilobots = - (1 - self.rho) * self.kilobots_dist(X_kilobots, Y_kilobots)
+        k_kilobots = self.kilobots_dist(X_kilobots, Y_kilobots)
         if self.light_dim:
-            k_light = - self.rho * self.light_dist(X_light, Y_light)
+            k_light = self.light_dist(X_light, Y_light)
         else:
             k_light = .0
         if self.weight_dim:
-            k_weight = - self.rho * self.weight_dist(X_weight, Y_weight)
+            k_weight = self.weight_dist(X_weight, Y_weight)
         else:
             k_weight = .0
         if self.action_dim:
-            k_action = - self.rho * self.action_dist(X_action, Y_action)
+            k_action = self.action_dist(X_action, Y_action)
         else:
             k_action = .0
 
         if return_components:
-            return (self.variance * np.exp(k_kilobots + k_light + k_weight + k_action),
+            return (self.variance * np.exp((self.rho - 1) * k_kilobots - self.rho * (k_light + k_weight + k_action)),
                     k_kilobots, k_light, k_weight, k_action)
-        return self.variance * np.exp(k_kilobots + k_light + k_weight + k_action)
+        return self.variance * np.exp((self.rho - 1) * k_kilobots - self.rho * (k_light + k_weight + k_action))
 
     def Kdiag(self, X):
         X_splits = np.split(X, np.cumsum([self.kilobots_dim, self.light_dim, self.weight_dim]), axis=1)
@@ -103,21 +148,21 @@ class KilobotEnvKernel(Kern):
         X_weight = X_splits[2]
         X_action = X_splits[3]
 
-        k_kilobots = -(1 - self.rho) * self.kilobots_dist.diag(X_kilobots)
+        k_kilobots = self.kilobots_dist.diag(X_kilobots)
         if self.light_dim:
-            k_light = -self.rho * self.light_dist.diag(X_light)
+            k_light = self.light_dist.diag(X_light)
         else:
             k_light = .0
         if self.weight_dim:
-            k_weight = -self.rho * self.weight_dist.diag(X_weight)
+            k_weight = self.weight_dist.diag(X_weight)
         else:
             k_weight = .0
         if self.action_dim:
-            k_action = -self.rho * self.action_dist.diag(X_action)
+            k_action = self.action_dist.diag(X_action)
         else:
             k_action = .0
 
-        return self.variance * np.exp(k_kilobots + k_light + k_weight + k_action)
+        return self.variance * np.exp((self.rho - 1) * k_kilobots - self.rho * (k_light + k_weight + k_action))
 
     def update_gradients_full(self, dL_dK, X, Y=None):
         X_splits = np.split(X, np.cumsum([self.kilobots_dim, self.light_dim, self.weight_dim]), axis=1)
@@ -158,21 +203,32 @@ class KilobotEnvKernel(Kern):
             self.action_bandwidth.gradient = np.sum(dK_a_d_bw, axis=(0, 1))
 
         # compute gradient w.r.t. rho
-        self.rho.gradient = np.sum(dL_dK * k * (-k_kilobots - k_light - k_weight - k_action))
+        self.rho.gradient = np.sum(dL_dK * k * (k_kilobots - k_light - k_weight - k_action))
 
         # compute gradient w.r.t. variance
         self.variance.gradient = np.sum(dL_dK * k) / self.variance
 
     def update_gradients_diag(self, dL_dKdiag, X):
         # compute gradient w.r.t. kernel bandwidths
-        # self.kilobots_bandwidth.gradient = np.zeros(2)
-        # self.light_bandwidth.gradient = np.zeros(self.light_dim)
-        # self.weight_bandwidth.gradient = np.zeros(self.weight_dim)
-        # self.action_bandwidth.gradient = np.zeros(self.action_dim)
+        self.kilobots_bandwidth.gradient = np.zeros(2)
+        if self.light_dim:
+            self.light_bandwidth.gradient = np.zeros(self.light_dim)
+        if self.weight_dim:
+            self.weight_bandwidth.gradient = np.zeros(self.weight_dim)
+        if self.action_dim:
+            self.action_bandwidth.gradient = np.zeros(self.action_dim)
         # compute gradient w.r.t. rho
-        # self.rho.gradient = 0
+        self.rho.gradient = 0
         # compute gradient w.r.t. variance
         self.variance.gradient = np.sum(dL_dKdiag)
+
+    def gradients_X(self, dL_dK, X, X2):
+        # return np.zeros((dL_dK.shape[0], 1))
+        pass
+
+    def gradients_X_diag(self, dL_dKdiag, X):
+        # return np.zeros((dL_dKdiag.shape[0], 1))
+        pass
 
     def __call__(self, X, Y=None):
         return self.K(X, Y)

kb_learning/learner/_learner.py
@@ -8,15 +8,15 @@ from typing import Generator
 from cluster_work import ClusterWork, InvalidParameterArgument
 
 from kb_learning.kernel import KilobotEnvKernel
-from kb_learning.kernel import KilobotEnvKernelWithWeight
 from kb_learning.kernel import EmbeddedSwarmDistance, MahaDist, MeanSwarmDist, MeanCovSwarmDist, PeriodicDist
-from kb_learning.kernel import compute_median_bandwidth, select_reference_set_by_kernel_activation, \
-    compute_mean_position_pandas, angle_from_swarm_mean, step_towards_center
+from kb_learning.kernel import compute_median_bandwidth, compute_median_bandwidth_kilobots, \
+    select_reference_set_by_kernel_activation, compute_mean_position_pandas, angle_from_swarm_mean, step_towards_center
 from kb_learning.ac_reps.lstd import LeastSquaresTemporalDifference
 from kb_learning.ac_reps.reps import ActorCriticReps
 from kb_learning.ac_reps.spwgp import SparseWeightedGP
+from kb_learning.ac_reps.gpy_spwgp import SparseWeightedGPyWrapper
 
-from kb_learning.envs.sampler import ParallelSARSSampler
+from kb_learning.envs.sampler import ParallelSARSSampler, SARSSampler
 from kb_learning.envs import register_object_env, register_gradient_light_object_env, \
     register_dual_light_complex_object_env
 
@@ -69,7 +69,8 @@ class ACRepsLearner(KilobotLearner):
             'bandwidth_factor_light': .55,
             'bandwidth_factor_action': .8,
             'bandwidth_factor_weight': .3,
-            'weight': .5,
+            'rho': .5,
+            'variance': .02 ** 2,
         },
         'learn_iterations': 1,
         'lstd': {
@@ -81,10 +82,7 @@ class ACRepsLearner(KilobotLearner):
             'epsilon': .3
         },
         'gp': {
-            'prior_variance': .02 ** 2,
             'noise_variance': 2e-5,
-            'min_variance': 1e-8,
-            'chol_regularizer': 1e-9,
             'num_sparse_states': 1000,
         },
         'eval': {
@@ -98,7 +96,7 @@ class ACRepsLearner(KilobotLearner):
         super().__init__()
 
         self.state_preprocessor = None
-        self.kernel = None
+        self.state_kernel = None
         self.state_action_kernel = None
 
         self.lstd = None
@@ -154,26 +152,34 @@ class ACRepsLearner(KilobotLearner):
         _extended_sars = self.sars.append(self.it_sars, ignore_index=True)
         # _extended_sars.reset_index(drop=True)
 
+        self.state_kernel, self.state_action_kernel = self._init_kernels()
+
         # compute kernel parameters
         logger.debug('computing kernel bandwidths.')
-        bandwidth_kb = compute_median_bandwidth(_extended_sars[self.kilobots_columns], sample_size=500,
-                                                preprocessor=self.state_preprocessor)
+        if self.state_preprocessor:
+            bandwidth_kb = compute_median_bandwidth(_extended_sars[self.kilobots_columns], sample_size=500,
+                                                    preprocessor=self.state_preprocessor)
+        else:
+            bandwidth_kb = compute_median_bandwidth_kilobots(_extended_sars[self.kilobots_columns], sample_size=500)
         bandwidth_kb *= self._params['kernel']['bandwidth_factor_kb']
-        self.kernel.kilobots_dist.set_bandwidth(bandwidth_kb)
+        self.state_kernel.kilobots_bandwidth = bandwidth_kb
+        self.state_action_kernel.kilobots_bandwidth = bandwidth_kb
 
         if self.light_columns is not None:
             bandwidth_l = compute_median_bandwidth(_extended_sars[self.light_columns], sample_size=500)
             bandwidth_l *= self._params['kernel']['bandwidth_factor_light']
-            self.kernel.light_dist.set_bandwidth(bandwidth_l)
+            self.state_kernel.light_bandwidth = bandwidth_l
+            self.state_action_kernel.light_bandwidth = bandwidth_l
 
         if self.weight_columns is not None:
             bandwidth_w = compute_median_bandwidth(_extended_sars[self.weight_columns], sample_size=500)
             bandwidth_w *= self._params['kernel']['bandwidth_factor_weight']
-            self.kernel.weight_dist.set_bandwidth(bandwidth_w)
+            self.state_kernel.weight_bandwidth = bandwidth_w
+            self.state_action_kernel.weight_bandwidth = bandwidth_w
 
         bandwidth_a = compute_median_bandwidth(_extended_sars['A'], sample_size=500)
         bandwidth_a *= self._params['kernel']['bandwidth_factor_action']
-        self.kernel.action_dist.set_bandwidth(bandwidth_a)
+        self.state_action_kernel.action_bandwidth = bandwidth_a
 
         logger.debug('selecting SARS samples.')
         if _extended_sars.shape[0] <= sampling_params['num_SARS_samples']:
@@ -187,7 +193,7 @@ class ACRepsLearner(KilobotLearner):
         logger.debug('selecting lstd samples.')
         lstd_reference = select_reference_set_by_kernel_activation(data=self.sars[['S', 'A']],
                                                                    size=self._params['lstd']['num_features'],
-                                                                   kernel_function=self.kernel,
+                                                                   kernel_function=self.state_kernel,
                                                                    batch_size=10)
 
         self.lstd_samples = self.sars.loc[lstd_reference][['S', 'A']]
@@ -195,14 +201,14 @@ class ACRepsLearner(KilobotLearner):
         def state_features(state):
             if state.ndim == 1:
                 state = state.reshape((1, -1))
-            return self.kernel(state, self.lstd_samples['S'].values)
+            return self.state_kernel(state, self.lstd_samples['S'].values)
 
         def state_action_features(state, action):
             if state.ndim == 1:
                 state = state.reshape((1, -1))
             if action.ndim == 1:
                 action = action.reshape((1, -1))
-            return self.kernel(np.c_[state, action], self.lstd_samples.values)
+            return self.state_action_kernel(np.c_[state, action], self.lstd_samples.values)
 
         for i in range(self._params['learn_iterations']):
             # compute features
@@ -247,8 +253,10 @@ class ACRepsLearner(KilobotLearner):
 
             # fit weighted GP to samples
             logger.info('fitting GP to policy')
-            self.policy = self._init_policy((self.sampler.env.action_space.low, self.sampler.env.action_space.high))
-            self.policy.train(self.sars['S'].values, self.sars['A'].values, weights, gp_samples)
+            # self.policy = self._init_policy(self.state_kernel,
+            #                                 (self.sampler.env.action_space.low, self.sampler.env.action_space.high))
+            self.policy.train(inputs=self.sars['S'].values, outputs=self.sars['A'].values, weights=weights,
+                              sparse_inputs=gp_samples, optimize=True)
 
             # evaluate policy
             logger.info('evaluating policy')
@@ -279,14 +287,17 @@ class ACRepsLearner(KilobotLearner):
     def reset(self, config: dict, rep: int) -> None:
         self._init_SARS()
 
-        self.kernel = self._init_kernels()
+        self.state_kernel, self.state_action_kernel = self._init_kernels()
 
         self.sampler = self._init_sampler()
 
-        self.policy = self._init_policy((self.sampler.env.action_space.low, self.sampler.env.action_space.high))
+        self.policy = self._init_policy(kernel=self.state_kernel, output_bounds=(self.sampler.env.action_space.low,
+                                                                                 self.sampler.env.action_space.high))
 
     def _init_kernels(self):
         kernel_params = self._params['kernel']
+        sampling_params = self._params['sampling']
+
         if kernel_params['kb_dist'] in ['kilobot', 'embedded']:
             kb_dist_class = EmbeddedSwarmDistance
         elif kernel_params['kb_dist'] == 'mean':
@@ -321,35 +332,31 @@ class ACRepsLearner(KilobotLearner):
         else:
             raise InvalidParameterArgument
 
-        if self._params['sampling']['w_factor'] is not None:
-            return KilobotEnvKernel(rho=kernel_params['weight'],
-                                    light_idx=2 * self._params['sampling']['num_kilobots'],
-                                    action_idx=2 * self._params['sampling']['num_kilobots'] +
-                                               self._light_dimensions,
-                                    kb_dist_class=kb_dist_class,
-                                    light_dist_class=l_dist_class,
-                                    action_dist_class=a_dist_class)
-        else:
-            return KilobotEnvKernelWithWeight(weight=kernel_params['weight'],
-                                              light_idx=2 * self._params['sampling']['num_kilobots'],
-                                              weight_idx=2 * self._params['sampling']['num_kilobots'] +
-                                                         self._light_dimensions,
-                                              action_idx=2 * self._params['sampling']['num_kilobots'] +
-                                                         self._light_dimensions + 1,
-                                              kb_dist_class=kb_dist_class,
-                                              light_dist_class=l_dist_class,
-                                              action_dist_class=a_dist_class,
-                                              weight_dist_class=w_dist_class)
+        state_kernel = KilobotEnvKernel(rho=kernel_params['rho'],
+                                        variance=kernel_params['variance'],
+                                        kilobots_dim=sampling_params['num_kilobots'] * 2,
+                                        light_dim=self._light_dimensions,
+                                        weight_dim=1 if sampling_params['w_factor'] is None else 0)
+        state_kernel.kilobots_dist = kb_dist_class()
+        state_kernel.light_dist = l_dist_class()
+        state_kernel.weight_dist = w_dist_class()
 
-    def _init_policy(self, action_bounds):
-        policy = SparseWeightedGP(kernel=self.kernel)
-        policy.gp_prior_variance = self._params['gp']['prior_variance']
-        policy.gp_noise_variance = self._params['gp']['noise_variance']
-        policy.gp_min_variance = self._params['gp']['min_variance']
-        policy.gp_cholesky_regularizer = self._params['gp']['chol_regularizer']
-        policy.output_bounds = action_bounds
-        policy.output_bounds_enforce = True
-        policy.gp_prior_mean = step_towards_center([-2, -1])
+        state_action_kernel = KilobotEnvKernel(rho=kernel_params['rho'],
+                                               variance=kernel_params['variance'],
+                                               kilobots_dim=sampling_params['num_kilobots'] * 2,
+                                               light_dim=self._light_dimensions,
+                                               weight_dim=1 if sampling_params['w_factor'] is None else 0,
+                                               action_dim=self._action_dimensions)
+        state_action_kernel.kilobots_dist = kb_dist_class()
+        state_action_kernel.light_dist = l_dist_class()
+        state_action_kernel.weight_dist = w_dist_class()
+        state_action_kernel.action_dist = a_dist_class()
+
+        return state_kernel, state_action_kernel
+
+    def _init_policy(self, kernel, output_bounds):
+        policy = SparseWeightedGPyWrapper(kernel=kernel, noise_var=self._params['gp']['noise_variance'],
+                                          mean_function=step_towards_center([-2, -1]), output_bounds=output_bounds)
 
         return policy
 
@@ -375,7 +382,7 @@ class ACRepsLearner(KilobotLearner):
 
     def _init_sampler(self):
         sampling_params = self._params['sampling']
-        return ParallelSARSSampler(object_shape=sampling_params['object_shape'],
+        return SARSSampler(object_shape=sampling_params['object_shape'],
                                    object_width=sampling_params['object_width'],
                                    object_height=sampling_params['object_height'],
                                    registration_function=register_object_env,
@@ -393,7 +400,17 @@ class ACRepsLearner(KilobotLearner):
         logger.info('pickling policy...')
         policy_file_name = os.path.join(self._log_path_rep, 'policy_it{:02d}.pkl'.format(self._it))
         with open(policy_file_name, mode='w+b') as policy_file:
-            pickle.dump(self.policy, policy_file)
+            pickle.dump(self.policy.to_dict(), policy_file)
+
+        # save kernels
+        logger.info('pickling kernel...')
+        state_kernel_file_name = os.path.join(self._log_path_rep, 'state_kernel_it{:02d}.pkl'.format(self._it))
+        with open(state_kernel_file_name, mode='w+b') as state_kernel_file:
+            pickle.dump(self.state_kernel.to_dict(), state_kernel_file)
+        state_action_kernel_file_name = os.path.join(self._log_path_rep, 'state_action_kernel_it{:02d}.pkl'.format(self._it))
+        with open(state_action_kernel_file_name, mode='w+b') as state_action_file:
+            pickle.dump(self.state_action_kernel.to_dict(), state_action_file)
+
 
         if self._params['sampling']['save_trajectories']:
             logger.info('pickling sampling trajectories...')
@@ -426,9 +443,20 @@ class ACRepsLearner(KilobotLearner):
         logger.info('restoring policy and kernel...')
         policy_file_name = os.path.join(self._log_path_rep, 'policy_it{:02d}.pkl'.format(n))
         with open(policy_file_name, mode='r+b') as policy_file:
-            self.policy = pickle.load(policy_file)
+            policy_dict = pickle.load(policy_file)
+        self.policy = SparseWeightedGPyWrapper.from_dict(policy_dict)
         self.sampler.policy = self.policy
-        self.kernel = self.policy.kernel
+
+        # restore kernel
+        logger.info('restoring policy and kernel...')
+        state_kernel_file_name = os.path.join(self._log_path_rep, 'state_kernel_it{:02d}.pkl'.format(n))
+        with open(state_kernel_file_name, mode='r+b') as state_kernel_file:
+            state_kernel_dict = pickle.load(state_kernel_file)
+        self.state_kernel = KilobotEnvKernel.from_dict(state_kernel_dict)
+        state_action_kernel_file_name = os.path.join(self._log_path_rep, 'state_action_kernel_it{:02d}.pkl'.format(n))
+        with open(state_action_kernel_file_name, mode='r+b') as state_action_kernel_file:
+            state_action_kernel_dict = pickle.load(state_action_kernel_file)
+        self.state_action_kernel = KilobotEnvKernel.from_dict(state_action_kernel_dict)
 
         if self._params['sampling']['save_trajectories']:
             logger.info('restoring sampling trajectories...')
@@ -479,8 +507,8 @@ class ACRepsLearner(KilobotLearner):
 
 
 class SampledWeightACRepsLearner(ACRepsLearner):
-    def _init_policy(self, action_bounds):
-        policy = super()._init_policy(action_bounds)
+    def _init_policy(self, output_bounds):
+        policy = super()._init_policy(output_bounds)
         policy.gp_prior_mean = step_towards_center([-3, -2])
 
         return policy
@@ -625,8 +653,8 @@ class GradientLightObjectACRepsLearner(ACRepsLearner):
         self._light_dimensions = 0
         self._action_dimensions = 1
 
-    def _init_policy(self, action_bounds):
-        policy = super()._init_policy(action_bounds)
+    def _init_policy(self, output_bounds):
+        policy = super()._init_policy(output_bounds)
         policy.gp_prior_mean = angle_from_swarm_mean(range(self._params['sampling']['num_kilobots'] * 2))
 
         return policy
@@ -750,8 +778,8 @@ class DualLightComplexObjectACRepsLearner(ACRepsLearner):
         self._light_dimensions = 2
         self._action_dimensions = 2
 
-    def _init_policy(self, action_bounds):
-        policy = super()._init_policy(action_bounds)
+    def _init_policy(self, output_bounds):
+        policy = super()._init_policy(output_bounds)
         policy.gp_prior_mean = angle_from_swarm_mean(range(self._params['sampling']['num_kilobots'] * 2), 2)
 
         return policy