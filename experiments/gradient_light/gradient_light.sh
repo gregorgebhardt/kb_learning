1a5cf04284c4237db2759d70a660ae91f5f08a92
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-05-02 18:06:12 +0200

    changed experiment

--- experiments/gradient_light/gradient_light.sh
+++ experiments/gradient_light/gradient_light.sh
@@ -7,10 +7,10 @@
 #SBATCH -e /home/yy05vipo/git/kb_learning/experiments/gradient_light_complex/l_%j.stderr
 #SBATCH -o /home/yy05vipo/git/kb_learning/experiments/gradient_light_complex/l_%j.stdout
 #
-#SBATCH -n 9               # Number of tasks
+#SBATCH -n 12               # Number of tasks
 #SBATCH -c 8                # Number of cores per task
 #SBATCH --mem-per-cpu=1000   # Main memory in MByte per MPI task
-#SBATCH -t 4:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
+#SBATCH -t 5:00:00         # Hours, minutes and seconds, or '#SBATCH -t 10' - only minutes
 #SBATCH -C avx2            # requires new nodes
 #SBATCH --hint=multithread
 
@@ -25,4 +25,4 @@ cd /home/yy05vipo/git/kb_learning/experiments
 
 srun hostname > $SLURM_JOB_ID.hostfile
 hostfileconv $SLURM_JOB_ID.hostfile -1
-job_stream --hostfile $SLURM_JOB_ID.hostfile.converted -- python gradient_light_complex/gradient_light_complex.py -c gradient_light_complex/gradient_light_complex.yml --log_level INFO -e square
+job_stream --hostfile $SLURM_JOB_ID.hostfile.converted -- python gradient_light/gradient_light.py -c gradient_light/gradient_light.yml --log_level INFO -e eval_height
