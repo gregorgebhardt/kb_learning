3c8100c0a8354971dd1af9b1fdc0e62481dd5dde
Unknown <max.huettenrauch@gmail.com>
2018-08-24 19:48:44 +0100

    evaluate ppo policy script

---
+++ kb_learning/learner/_ppo_eval.py
@@ -0,0 +1,33 @@
+import cloudpickle
+import gym
+from kb_learning.envs import register_object_env, NormalizeActionWrapper
+
+
+def main():
+    with open('/tmp/make_model.pkl', 'rb') as fh:
+        make_model = cloudpickle.load(fh)
+
+    model = make_model()
+
+    model.load('/tmp/00100')
+
+    env_id = register_object_env(.0, 15, 'quad', .15, .15, 'circular', .3)
+    env = NormalizeActionWrapper(gym.make(env_id))
+
+    obs = env.reset()
+    states = model.initial_state
+    dones = False
+
+    for _ in range(2000):
+        actions, values, states, neglogpacs = model.eval_step(obs, S=states, M=dones)
+
+        obs[:], rewards, dones, infos = env.step(actions[0])
+
+        if dones is True:
+            env.reset()
+
+        env.render()
+
+
+if __name__ == '__main__':
+    main()
