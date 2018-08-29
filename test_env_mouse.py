9df3087183f8ec842c4035141600b3fd22875abd
Gregor Gebhardt <gregor.gebhardt@gmail.com>
2018-08-29 11:17:31 +0100

    added test script for controlling swarm with the pointer.

---
+++ test_env_mouse.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from kb_learning.envs import ObjectRelativeEnv, register_object_relative_env, register_object_absolute_env
+
+import gym
+
+env_id = register_object_absolute_env(10, 'corner_quad', .15, .15, 'circular', .15)
+# env_id = register_object_relative_env(1., 10, 'quad', .15, .15, 'circular', .15)
+env: ObjectRelativeEnv = gym.make(env_id)
+
+_reward = 0
+for j in range(5):
+    env.reset()
+    env.render()
+    ep_r = 0
+    for i in range(350):
+        m_pos = env._screen.get_mouse_position()
+        # print(m_pos)
+        l_pos = env.get_light().get_state()
+        obs, r, done, info = env.step(np.array(m_pos) - l_pos)
+        ep_r += r
+        if done:
+            print('done!')
+            break
+
+    print(ep_r)
+    _reward += ep_r
+
+print('mean reward/episode: {}'.format(_reward/5))
